ba23f282fc3bd80050e2df67162f83fad543697a
Ai With Enoch <aiwithenoch@gmail.com>
2026-08-01 12:42:19 +0000

    feat: connect routes to persistent tender workspace

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { RouterProvider } from 'react-router';
 import router from './routes/Router';
 import './css/globals.css';
+import { TenderWorkspaceProvider } from './context/tender/TenderWorkspaceContext';
 
 function App() {
   return (
-    <>
-    
+    <TenderWorkspaceProvider>
       <RouterProvider router={router} />
-    </>
+    </TenderWorkspaceProvider>
   );
 }
 
